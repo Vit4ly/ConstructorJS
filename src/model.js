426e7616a0ebb94002a1b584d520c8012f3c6c05
Vitaliy <vit4ly.kiselev@gmail.com>
2020-08-22 18:45:08 +0300

    Final Project on Hosting second commit

--- src/model.js
+++ src/model.js
@@ -1,5 +1,5 @@
 import image from './assets/image.png'
-import { ImageBlock, TextBlock, TextColumnsBlock, TitleBlock} from "./Classes/blocks";
+import {ImageBlock, TextBlock, TextColumnsBlock, TitleBlock} from "./Classes/blocks";
 import {css} from "./utils";
 
 const text = `
@@ -13,24 +13,38 @@ export const model = [
             background: "linear-gradient(to right, #000, #eb7dfa)",
             color: "#fff",
             'text-align': "center",
-             padding: "1.5rem"
+            padding: "1.5rem"
         })
 
     }),
     new ImageBlock(image, {
-        styles: 'padding: 2rem 0;display: flex; justify-content: center;',
+
+        styles: css({
+            padding: '2rem 0',
+            display: 'flex',
+            "justify-content": 'center',
+        }),
         alt: 'my image',
         imageStyles: 'width: 500px; height: auto;'
     }),
     new TextBlock(text, {
-        styles: 'text-align: center; padding: 2rem; background-image: linear-gradient(to top right, #000, #8d7dfa ); color: #fff;'
+        styles: css({
+            "text-align": 'center',
+            padding: '2rem',
+            'background-image': 'linear-gradient(to top right, #000, #8d7dfa )',
+            color: '#fff'
+        }),
     }),
     new TextColumnsBlock([
         'This tutorial project is created in pure JavaScript without using any third party libraries!',
         'When creating this project , I learned how SOLID and OOP works in JavaScript',
-        'Так же получил опыт зодания UI своими руками'
+        'I also got experience in creating UI With my own hands!'
     ], {
-        styles: 'color:#fff; background-image: linear-gradient(to top right, #000, #8d7dfa);padding: 1rem'
+        styles: css({
+            color: '#fff',
+            'background-image': 'linear-gradient(to top right, #000, #8d7dfa)',
+            padding: ' 1rem',
+        })
     }),
 
 
